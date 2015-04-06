aa02dfeef44429c249b6925f60bf7354238dcc83
sankait36 <user@Sankait.mobile.asu.edu>
2015-04-06 10:46:26 -0700

    Split functionality into different classes

---
+++ src/LoginScreen.java
@@ -0,0 +1,123 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.*;
+
+import jxl.*;
+import jxl.write.*;
+import jxl.read.biff.*;
+
+public class LoginScreen extends JFrame implements ActionListener
+{
+	JTextField userNameTextField;
+	JPasswordField passwordPasswordField ;
+	
+	public LoginScreen()
+	{
+		super ("Efferent Patient Care System - Login");
+		
+		setLocationRelativeTo(null);
+		setLayout(null);
+		
+		userNameTextField = new JTextField("");
+		passwordPasswordField = new JPasswordField("");
+		
+		JLabel userNameLabel = new JLabel("Username");
+		userNameLabel.setBounds(50, 25, 75, 25);
+		add(userNameLabel);
+		
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(50, 60, 75, 25);
+		add(passwordLabel);
+		
+		JLabel newUserLabel = new JLabel("New User? Click Register!");
+		newUserLabel.setBounds(65, 130, 150, 25);
+		add(newUserLabel);
+		
+		userNameTextField.requestFocus();
+		userNameTextField.setText("");
+		userNameTextField.setBounds(125, 25, 110, 25);
+		add(userNameTextField);	
+		
+		passwordPasswordField.setText("");
+		passwordPasswordField.setBounds(125, 60, 110, 25);
+		passwordPasswordField.setEchoChar('*');
+		add(passwordPasswordField);
+		
+		JButton loginButton = new JButton("Login");
+		loginButton.setBounds(50, 100, 75, 25);
+		loginButton.setActionCommand("Login");
+		loginButton.addActionListener(this);
+		add(loginButton);
+		
+		JButton newUserButton = new JButton("Register");
+		newUserButton.setBounds(150, 100, 85, 25);
+		newUserButton.setActionCommand("NewUser");
+		newUserButton.addActionListener(this);
+		add(newUserButton);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		if("Login".equals(arg0.getActionCommand())) 
+		{
+			try 
+			{
+				LoginUser();
+			} 
+			catch (BiffException | WriteException | IOException e) 
+			{
+				e.printStackTrace();
+			}	
+		}
+		
+		else if ("NewUser".equals(arg0.getActionCommand())) 
+		{
+			userNameTextField.setText("");
+			passwordPasswordField.setText("");
+			new RegisterScreen();
+		}
+	}
+	
+	public void LoginUser() throws BiffException, IOException, WriteException{
+		
+		JPanel alert = new JPanel();		
+		// Define file name
+		String fileName = userNameTextField.getText() + ".xls";
+		
+		// Try to read excel file
+		try
+		{
+			Workbook workbook = Workbook.getWorkbook(new File(fileName));
+			Sheet sheet = workbook.getSheet(0);
+			
+	        // Get username and password from file
+	        Cell userNameCell = sheet.getCell(0,0);
+	        Cell passwordCell = sheet.getCell(1,0);
+	        
+	        // Convert to string
+	        String userName = userNameCell.getContents();
+	        String password = passwordCell.getContents();
+	        
+	        // Login
+	        if (userName.equals(userNameTextField.getText()) && (password.equals(String.valueOf(passwordPasswordField.getPassword())))) 
+	        {
+	    		JOptionPane.showMessageDialog(alert, "Login successful!");
+	    		
+	        } 
+	        else 
+	        {
+	        	JOptionPane.showMessageDialog(alert, "Password incorrect.");	
+	        }
+	        	
+		}
+		catch (FileNotFoundException notFound) 
+		{
+    		JOptionPane.showMessageDialog(alert, "Username invalid.");	
+		}
+	}
+}
---
+++ src/RegisterScreen.java
@@ -0,0 +1,342 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.*;
+
+import jxl.*;
+import jxl.write.*;
+import jxl.read.biff.*;
+
+public class RegisterScreen extends JFrame implements ActionListener
+{
+	JTextField newUserNameTextField;
+	JTextField firstNameTextField;
+	JTextField lastNameTextField;
+	JTextField emailTextField;
+	JPasswordField newPasswordPasswordField;
+	JPasswordField confirmPasswordPasswordField;
+	JRadioButton patientRadioButton;
+	JRadioButton doctorRadioButton;
+	JLabel ageLabel;
+	JTextField ageTextField;	
+	JRadioButton maleRadioButton;
+	JRadioButton femaleRadioButton;
+	JButton registerButton;
+	
+	public RegisterScreen()
+	{
+		super("Efferent Patient Care System - Register");
+		
+		setResizable(false);
+		setSize(400, 355);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		
+		newUserNameTextField = new JTextField("");
+		firstNameTextField = new JTextField("");
+		lastNameTextField = new JTextField("");
+		emailTextField = new JTextField("");
+		newPasswordPasswordField = new JPasswordField("");
+		confirmPasswordPasswordField = new JPasswordField("");
+		
+		patientRadioButton = new JRadioButton("Patient");
+		doctorRadioButton = new JRadioButton("Practitioner");
+
+		ageLabel = new JLabel("Age");
+		ageTextField = new JTextField("");
+		
+		maleRadioButton = new JRadioButton("Male");
+		femaleRadioButton = new JRadioButton("Female");	
+		
+		registerButton = new JButton("Register");
+		
+		
+		// Username Label
+				JLabel userNameLabel = new JLabel("Username");
+				userNameLabel.setBounds(50, 25, 75, 25);
+				add(userNameLabel);
+				
+				// Username Text Field
+				newUserNameTextField.setText("");
+				newUserNameTextField.setBounds(175, 25, 160, 25);
+				add(newUserNameTextField);
+				
+				// First Name Label
+				JLabel firstNameLabel = new JLabel("First Name");
+				firstNameLabel.setBounds(50, 60, 75, 25);
+				add(firstNameLabel);
+			
+				// First Name Text Field
+				firstNameTextField.setText("");
+				firstNameTextField.setBounds(175, 60, 160, 25);
+				add(firstNameTextField);
+
+				// Last Name Label
+				JLabel lastNameLabel = new JLabel("Last Name");
+				lastNameLabel.setBounds(50, 95, 75, 25);
+				add(lastNameLabel);
+				
+				// Last Name Text Field
+				lastNameTextField.setText("");
+				lastNameTextField.setBounds(175, 95, 160, 25);
+				add(lastNameTextField);
+				
+				// Email Label
+				JLabel emailLabel = new JLabel("Email Address");
+				emailLabel.setBounds(50, 130, 100, 25);
+				add(emailLabel);
+			
+				// Email Text Field
+				emailTextField.setText("");
+				emailTextField.setBounds(175, 130, 160, 25);
+				add(emailTextField);
+				
+				// Password Label
+				JLabel passwordLabel = new JLabel("Enter Password");
+				passwordLabel.setBounds(50, 165, 125, 25);
+				add(passwordLabel);
+				
+				// Password Password Field
+				newPasswordPasswordField.setText("");
+				newPasswordPasswordField.setBounds(175, 165, 160, 25);
+				newPasswordPasswordField.setEchoChar('*');
+				add(newPasswordPasswordField);
+				
+				// Confirm Password Label
+				JLabel confirmPasswordLabel = new JLabel("Confirm Password");
+				confirmPasswordLabel.setBounds(50, 200, 125, 25);
+				add(confirmPasswordLabel);
+
+				// Confirm Password Password Field
+				confirmPasswordPasswordField.setText("");
+				confirmPasswordPasswordField.setBounds(175, 200, 160, 25);
+				confirmPasswordPasswordField.setEchoChar('*');
+				add(confirmPasswordPasswordField);
+				
+				// Age Label
+				ageLabel.setBounds(50, 270, 125, 25);
+				add(ageLabel);
+				ageLabel.setVisible(false);
+				
+				// Age Text Field
+				ageTextField.setText("");
+				ageTextField.setBounds(175, 270, 160, 25);
+				add(ageTextField);
+				ageTextField.setVisible(false);
+				
+				// Male Radio Button
+				maleRadioButton.setSelected(false);
+				maleRadioButton.setBounds(50, 305, 100, 25);
+				maleRadioButton.setActionCommand("Male");
+				maleRadioButton.addActionListener(this);
+				add(maleRadioButton);
+				maleRadioButton.setVisible(false);
+						
+				// Female Radio Button
+				femaleRadioButton.setSelected(false);
+				femaleRadioButton.setBounds(150, 305, 100, 25);
+				femaleRadioButton.setActionCommand("Female");
+				femaleRadioButton.addActionListener(this);
+				add(femaleRadioButton);
+				femaleRadioButton.setVisible(false);
+				
+				// Patient Radio Button
+				patientRadioButton.setSelected(false);
+				patientRadioButton.setBounds(50, 235, 100, 25);
+				patientRadioButton.setActionCommand("Patient");
+				patientRadioButton.addActionListener(this);
+				add(patientRadioButton);
+				
+				// Doctor Radio Button
+				doctorRadioButton.setSelected(false);
+				doctorRadioButton.setBounds(150, 235, 100, 25);
+				doctorRadioButton.setActionCommand("Doctor");
+				doctorRadioButton.addActionListener(this);
+				add(doctorRadioButton);
+
+				// Register Button
+				registerButton.setBounds(50, 270, 285, 25);
+				registerButton.setActionCommand("Register");
+				registerButton.addActionListener(this);
+				add(registerButton);
+
+				setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		if("Patient".equals(arg0.getActionCommand())) 
+		{
+			patientRadioButton.setSelected(true);
+			setSize(400, 425);
+			doctorRadioButton.setSelected(false);
+			registerButton.setBounds(50, 340, 285, 25);
+			ageLabel.setVisible(true);
+			ageTextField.setVisible(true);
+			ageTextField.setText("");
+			ageTextField.requestFocus();
+			maleRadioButton.setVisible(true);
+			maleRadioButton.setSelected(false);
+			femaleRadioButton.setVisible(true);
+			femaleRadioButton.setSelected(false);
+		}
+		
+		else if ("Doctor".equals(arg0.getActionCommand())) 
+		{
+			doctorRadioButton.setSelected(true);
+			setSize(400, 355);
+			patientRadioButton.setSelected(false);
+			registerButton.setBounds(50, 270, 285, 25);
+			ageLabel.setVisible(false);
+			maleRadioButton.setVisible(false);
+			femaleRadioButton.setVisible(false);
+			ageTextField.setVisible(false);
+		} 
+		else if ("Male".equals(arg0.getActionCommand())) 
+		{
+			femaleRadioButton.setSelected(false);
+			maleRadioButton.setSelected(true);
+		} 
+		else if ("Female".equals(arg0.getActionCommand()))
+		{
+			maleRadioButton.setSelected(false);
+			femaleRadioButton.setSelected(true);
+		} 
+		else if ("Register".equals(arg0.getActionCommand())) 
+		{
+			try
+			{
+				validateRegistrationInfo();
+			} 
+			catch (BiffException|WriteException|IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	public void RegisterNewUser() {
+		
+		User newUser;
+		
+		// Set Patient attributes
+		if (patientRadioButton.isSelected()) {
+			
+			String gender;
+			int patientID = 123456789;										// Patient ID TBD
+			int age = Integer.parseInt(ageTextField.getText());
+
+			// Set gender
+			if (maleRadioButton.isSelected()) {
+				gender = "Male";
+			} else {
+				gender = "Female";
+			}
+			
+			newUser = new Patient(age, gender, patientID);
+
+		// Set Doctor attributes
+		} else {
+			
+			newUser = new Doctor();
+		}
+		
+		// Assign all user information
+		newUser.setUserName(newUserNameTextField.getText());
+		newUser.setFirstName(firstNameTextField.getText());
+		newUser.setLastName(lastNameTextField.getText());
+		newUser.setEmail(emailTextField.getText());
+		newUser.setPassword(newPasswordPasswordField.getPassword());
+		
+		// Try to create user Excel file
+		try {
+			createNewUserFile(newUser);
+		} catch (BiffException|WriteException|IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void validateRegistrationInfo() throws BiffException, IOException, WriteException{
+		
+		JFrame alert = new JFrame();
+		
+		// Check for incomplete information
+		if ("".equals(newUserNameTextField.getText()) ||
+		   ("".equals(firstNameTextField.getText())) ||
+		   ("".equals(lastNameTextField.getText())) ||
+		   ("".equals(emailTextField.getText())) ||
+		   ("".equals(String.valueOf(newPasswordPasswordField.getPassword()))) ||
+		   ("".equals(String.valueOf(confirmPasswordPasswordField.getPassword()))) ||
+		   !(patientRadioButton.isSelected() || doctorRadioButton.isSelected()) ||
+		   (patientRadioButton.isSelected() && ("".equals(ageTextField.getText()) ||
+				                               !(maleRadioButton.isSelected() || femaleRadioButton.isSelected())))) {
+			JOptionPane.showMessageDialog(alert, "Please complete all fields.");
+			return;
+		}
+		
+		
+		// Check if username is already taken
+		File f = new File(newUserNameTextField.getText() + ".xls");
+		if (f.exists()) {
+			JOptionPane.showMessageDialog(alert, "Username already taken. Please choose another.");
+			return;
+		}
+
+		//Check password equivalency
+		if (!String.valueOf(newPasswordPasswordField.getPassword()).equals(String.valueOf(confirmPasswordPasswordField.getPassword()))) {
+			JOptionPane.showMessageDialog(alert, "Passwords do not match.");	
+			return;
+		}
+		
+		// No discrepancies in user information, attempt to register
+		RegisterNewUser();
+	}
+	
+	public void createNewUserFile(User newUser) throws BiffException, IOException, WriteException
+	{		
+		// Define filename
+		String fileName = newUser.getUserName() + ".xls";
+		
+		// Create new file
+        WritableWorkbook workbook = Workbook.createWorkbook(new File(fileName));
+        WritableSheet sheet = workbook.createSheet(newUser.getUserName(), 0);
+ 
+        // Add user information to cells
+        Label label = new Label(0, 0, newUser.getUserName());
+        sheet.addCell(label);
+        label = new Label(1, 0, String.valueOf(newUser.getPassword()));
+        sheet.addCell(label);
+        label = new Label(2, 0, newUser.getFirstName());
+        sheet.addCell(label);
+        label = new Label(3, 0, newUser.getLastName());
+        sheet.addCell(label);
+        label = new Label(4, 0, newUser.getEmail());
+        sheet.addCell(label);
+
+        
+        // Try to access Patient specific methods if user is Patient
+        try
+        {
+	        label = new Label(5, 0, String.valueOf(((Patient) newUser).getAge()));
+	        sheet.addCell(label);
+	        label = new Label(6, 0, String.valueOf(((Patient) newUser).getGender()));
+	        sheet.addCell(label);
+	        label = new Label(7, 0, String.valueOf(((Patient) newUser).getID()));
+	        sheet.addCell(label);
+        } 
+        catch(ClassCastException myException) { myException.printStackTrace(); }
+        
+        workbook.write();
+        workbook.close();
+
+        // Show registration confirmation
+		JFrame alert = new JFrame();
+		JOptionPane.showMessageDialog(alert, "Registration successful!");		
+
+		// Show login window
+		new LoginScreen();
+	}
+
+}
