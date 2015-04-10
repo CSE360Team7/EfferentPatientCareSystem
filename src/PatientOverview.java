c3e38d97f2795e135f599d799a2948bdc9b2e591
sankait36 <sankait36@hotmail.com>
2015-04-10 10:58:15 -0700

    Update PatientOverview.java

--- src/PatientOverview.java
+++ src/PatientOverview.java
@@ -5,5 +5,7 @@ public class PatientOverview extends JFrame
 	public PatientOverview()
 	{
 		super("Efferent Patient Care System - Patient Overview");
+		setSize(500,500);
+		setVisible(true);
 	}
 }
