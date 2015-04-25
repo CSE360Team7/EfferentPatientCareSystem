01dfe4b413b5acaace8d1a9b992abd569f5fbb10
AMRONDEAU <adrien.rondeau@gmail.com>
2015-04-25 12:51:55 -0700

    Fixed EXCEL files

--- src/PatientOverview.java
+++ src/PatientOverview.java
@@ -62,7 +62,6 @@ public class PatientOverview extends JFrame
 		this.add(welcome);
 		
 		recentMessages = new JLabel("Your Recent Messages");
-		recentMessages.setFont();
 		recentMessages.setBounds(10, 55, 150, 15);
 		this.add(recentMessages);
 				
